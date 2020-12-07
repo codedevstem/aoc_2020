0e8092bfaa3c921fe86b3ce8b839bd011a6d4f08
Kristian Hamre-Os <codedevstem@gmail.com>
2020-12-07 14:01:59 +0100

    Solves day_07/part_2

---
+++ day_07/part_2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+const filepath = "day_07/part_2/input.txt"
+
+var bagPattern = regexp.MustCompile(`(^|(\d+)\s)(\b(\w+\s\w+)\s(bag(\s|s\s|,|s,|\.|s\.)))`)
+
+type BagTuple struct {
+	name     string
+	contains int
+}
+
+func main() {
+	start := time.Now()
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	read := time.Now()
+
+	scanner := bufio.NewScanner(file)
+	bagMap := map[string][]BagTuple{}
+	for scanner.Scan() {
+		raw := scanner.Text()
+		matches := bagPattern.FindAllStringSubmatch(raw, -1)
+		if len(matches) < 2 {
+			continue
+		}
+		for i := 1; i < len(matches); i++ {
+			numberOfBagsToContain, _ := strconv.Atoi(matches[i][2])
+			bagMap[matches[0][4]] = append(bagMap[matches[0][4]], BagTuple{matches[i][4], numberOfBagsToContain})
+		}
+	}
+	p1 := time.Now()
+
+	var numberOfBagsToHold = 0
+	numberOfBagsToHold += CalculateNrBagsToHoldForKey(bagMap["shiny gold"], bagMap)
+	fmt.Printf("Read: %v\n", time.Now().Sub(read))
+	fmt.Printf("P1: %v\n", time.Now().Sub(p1))
+	fmt.Printf("Total: %v\n", time.Now().Sub(start))
+	fmt.Printf("Sum can contain: %d\n", numberOfBagsToHold)
+}
+
+func CalculateNrBagsToHoldForKey(tuples []BagTuple, bagMap map[string][]BagTuple) int {
+	nrForBag := 0
+	for _, tuple := range tuples {
+		nrForBag += tuple.contains * (1 + CalculateNrBagsToHoldForKey(bagMap[tuple.name], bagMap))
+	}
+	return nrForBag
+}
